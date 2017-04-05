cd40b0c5aaa55fe14f175c19dd300554c9167168
Mateus Menezes <mateusmenezes95@gmail.com>
2017-04-05 09:04:01 -0300

    Move to point added

--- src/movCartesiano.py
+++ src/movCartesiano.py
@@ -45,7 +45,10 @@ class turtle():
         cos_angle = (dot_product / (ref_vector_abs * comp_vector_abs))
         angle = math.acos(cos_angle)
 
-        angular_speed = 10*2*math.pi/360
+        if(self.user_pose.y >= self.pose.y):
+            angular_speed = 45*2*math.pi/360
+        else:
+            angular_speed = -45*2*math.pi/360
 
         #We wont use linear components
         self.vel_msg.linear.x=0
@@ -54,12 +57,12 @@ class turtle():
         self.vel_msg.angular.x = 0
         self.vel_msg.angular.y = 0
 
-        self.vel_msg.angular.z = abs(angular_speed)
+        self.vel_msg.angular.z = angular_speed
         # Setting the current time for distance calculus
         t0 = rospy.Time.now().to_sec()
         current_angle = 0
 
-        while(current_angle < angle):
+        while(abs(current_angle) < angle):
             self.velocity_publisher.publish(self.vel_msg)
             t1 = rospy.Time.now().to_sec()
             current_angle = angular_speed*(t1-t0)
@@ -69,8 +72,24 @@ class turtle():
         #Forcing our robot to stop
         self.vel_msg.angular.z = 0
         self.velocity_publisher.publish(self.vel_msg)
-        rospy.spin()
 
+    def move2point(self):
+        while math.sqrt(math.pow((self.user_pose.x - self.pose.x), 2) + math.pow((self.user_pose.y - self.pose.y), 2)) >= self.distance_tolerance:
+
+            #Porportional Controller
+            #linear velocity in the x-axis:
+            self.vel_msg.linear.x = 1.5 * math.sqrt(math.pow((self.user_pose.x - self.pose.x), 2) + math.pow((self.user_pose.y - self.pose.y), 2))
+            self.vel_msg.linear.y = 0
+            self.vel_msg.linear.z = 0
+            print self.vel_msg.linear.x
+
+            #Publishing our vel_msg
+            self.velocity_publisher.publish(self.vel_msg)
+            self.rate.sleep()
+        #Stopping our robot after the movement is over
+        self.vel_msg.linear.x = 0
+        self.velocity_publisher.publish(self.vel_msg)
+        rospy.spin()
 
 if __name__ == '__main__':
     try:
@@ -78,6 +97,7 @@ if __name__ == '__main__':
         x = turtle()
         x.userInput()
         x.rotate()
+        x.move2point()
 
     except rospy.ROSInterruptException: pass
 
