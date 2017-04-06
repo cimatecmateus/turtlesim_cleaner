8d43b14f728c1ee38c122c5274b9221a28e7dc15
Mateus Menezes <mateusmenezes95@gmail.com>
2017-04-06 19:49:16 -0300

    Multiple points added

--- src/go2Goal.py
+++ src/go2Goal.py
@@ -16,8 +16,10 @@ class turtle():
         self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.callback)
         #Instance of class Pose
         self.pose = Pose()
+        #Instance of class Twist
+        self.vel_msg = Twist()
         #Define frequency loop to 10Hz
-        self.rate = rospy.Rate(55)
+        self.rate = rospy.Rate(65)
     
     #Callback function to read data from /turtle1/Pose topic
     def callback(self, data):
@@ -33,12 +35,8 @@ class turtle():
         self.user_pose.x = input("Set your x goal: ")
         self.user_pose.y = input("Set your y goal: ")
         self.distance_tolerance = input("Set your tolerance: ")
-        #Instance of class Twist
-        self.vel_msg = Twist()
 
-    def rotate(self):
-        ref_vector = np.array([1,0])
-        comp_vector = np.array([self.user_pose.x - self.pose.x, self.user_pose.y - self.pose.y])
+    def rotate(self, ref_vector, comp_vector):
         dot_product = np.dot(ref_vector, comp_vector)
         ref_vector_abs = np.sqrt((ref_vector*ref_vector).sum())
         comp_vector_abs = np.sqrt((comp_vector*comp_vector).sum())
@@ -56,9 +54,9 @@ class turtle():
         self.vel_msg.linear.z=0
         self.vel_msg.angular.x = 0
         self.vel_msg.angular.y = 0
-
+        #Set the angular velocity
         self.vel_msg.angular.z = angular_speed
-        # Setting the current time for distance calculus
+        #Setting the current time for distance calculus
         t0 = rospy.Time.now().to_sec()
         current_angle = 0
 
@@ -69,7 +67,7 @@ class turtle():
             self.rate.sleep()
             print current_angle
 
-        #Forcing our robot to stop
+        #Forcing the turtle to stop
         self.vel_msg.angular.z = 0
         self.velocity_publisher.publish(self.vel_msg)
 
@@ -78,7 +76,7 @@ class turtle():
 
             #Porportional Controller
             #linear velocity in the x-axis:
-            self.vel_msg.linear.x = 1.5 * math.sqrt(math.pow((self.user_pose.x - self.pose.x), 2) + math.pow((self.user_pose.y - self.pose.y), 2))
+            self.vel_msg.linear.x = 2 * math.sqrt(math.pow((self.user_pose.x - self.pose.x), 2) + math.pow((self.user_pose.y - self.pose.y), 2))
             self.vel_msg.linear.y = 0
             self.vel_msg.linear.z = 0
             print self.vel_msg.linear.x
@@ -89,15 +87,28 @@ class turtle():
         #Stopping our robot after the movement is over
         self.vel_msg.linear.x = 0
         self.velocity_publisher.publish(self.vel_msg)
-        rospy.spin()
+        #rospy.spin()
+
+    def run(self):
+        #Initial vector of direction reference
+        ref_vector = np.array([1,0])
+        while True:
+            #Take the parameters os user
+            self.userInput()
+            #Vector that point to direction
+            dir_vector = np.array([self.user_pose.x - self.pose.x, self.user_pose.y - self.pose.y])
+            #Rotate to direction specified
+            self.rotate(ref_vector,dir_vector)
+            #Move to point specified for user
+            self.move2point()
+            #Change the vector direction
+            ref_vector = dir_vector
 
 if __name__ == '__main__':
     try:
         #Testing our function
         x = turtle()
-        x.userInput()
-        x.rotate()
-        x.move2point()
+        x.run()
 
     except rospy.ROSInterruptException: pass
 
